6f47afea12e867c03308b49ae4631a04be3b922f
ekaterina <ekaterina.redekop@philips.com>
2020-11-19 19:46:34 +0300

    added gpu_num to argument

--- main.py
+++ main.py
@@ -2,42 +2,66 @@ import numpy as np
 from unc_estimation import SUE_Ensemble, SUE_TTA, SUE_MCDO
 import argparse
 import os
-from main_utils import parse, load_image, load_model, to_tensor
+import torch
+from main_utils import parse, load_image, load_model, to_tensor, load_model_DO, model_parallel
+import tqdm
+import torch.backends.cudnn as cudnn
+import random
 
 
 def main():
-
     parser = argparse.ArgumentParser()
     args = parse(parser)
+
+    os.environ["CUDA_VISIBLE_DEVICES"] = args.gpu_number
+    cudnn.benchmark = False
+    cudnn.deterministic = True
+    random.seed(1)
+    np.random.seed(1)
+    torch.manual_seed(1)
+    torch.cuda.manual_seed(1)
+
     """Make folder to save results"""
     if not os.path.exists(args.result_path):
         os.makedirs(args.result_path)
 
-    """Load image, transform to tensor and add to batch"""
+    """Find out number of channels in images"""
     image_path = args.image_path
-    im = to_tensor(load_image(image_path)).cuda()
-    model_path = args.model_path
+    image_path_idxs = [k for k in os.listdir(image_path)]
+    im_tmp = load_image(image_path + image_path_idxs[0])
+    n_channels = im_tmp.shape[2]
 
     """If the desired SUE method - Ensemble, user provide folder to ensemble of saved models, which are loaded to 
-    list of models """
+            list of models """
+    model_path = args.model_path
     if args.unc_method == 'Ensemble':
         model = []
         paths = [k for k in os.listdir(model_path)]
         for _path in paths:
-            model.append(load_model(model_path + _path, _n_channels=im.shape[1], _n_classes=1).cuda())
-    else:
-        model = load_model(model_path, _n_channels=im.shape[1], _n_classes=1).cuda()
-
-    """Calculate uncertainty with the desired method"""
-    if args.unc_method == 'TTA':
-        aleatoric, epistemic = SUE_TTA(model, batch=im, last_layer=False)
-    elif args.unc_method == 'Ensemble':
-        aleatoric, epistemic = SUE_Ensemble(model, batch=im, last_layer=False)
+            model.append(model_parallel(load_model(model_path + _path, _n_channels=n_channels, _n_classes=1).cuda()))
+    elif args.unc_method == 'TTA':
+        model = model_parallel(load_model(model_path, _n_channels=n_channels, _n_classes=1).cuda())
     elif args.unc_method == 'MCDO':
-        aleatoric, epistemic = SUE_MCDO(model, batch=im, last_layer=False, T=10)
+        model = model_parallel(load_model_DO(model_path, _n_channels=n_channels, _n_classes=1).cuda())
 
-    np.save(args.result_path + 'aleatoric.npy', aleatoric)
-    np.save(args.result_path + 'epistemic.npy', epistemic)
+    tq = tqdm.tqdm(total=(len(image_path_idxs)))
+    for _image_path_idxs in image_path_idxs:
+        tq.update(1)
+        """Load images, transform to tensor and add to batch"""
+        im = to_tensor(load_image(image_path + _image_path_idxs))
+        im = torch.from_numpy(np.array(im)).cuda()
+
+        """Calculate uncertainty with the desired method"""
+        if args.unc_method == 'TTA':
+            aleatoric, epistemic = SUE_TTA(model, batch=im, last_layer=False)
+        elif args.unc_method == 'Ensemble':
+            aleatoric, epistemic = SUE_Ensemble(model, batch=im, last_layer=False)
+        elif args.unc_method == 'MCDO':
+            aleatoric, epistemic = SUE_MCDO(model, batch=im, last_layer=False, T=10)
+
+        """Save images to result_path"""
+        np.save(args.result_path + _image_path_idxs.split('.')[0] + '_aleatoric.npy', aleatoric)
+        np.save(args.result_path + _image_path_idxs.split('.')[0] + '_epistemic.npy', epistemic)
     print('Done!')
 
 
--- main_utils.py
+++ main_utils.py
@@ -1,6 +1,8 @@
 import cv2
 import torch
+import torch.nn as nn
 from models2D.unet import UNet
+from models2D.unet_do import UNet_DO
 import numpy as np
 
 
@@ -10,6 +12,7 @@ def parse(parser):
     arg('--model_path', type=str)
     arg('--result_path', type=str)
     arg('--unc_method', type=str, choices=['TTA', 'Ensemble', 'MCDO'])
+    arg('--gpu_number', type=str)
     args = parser.parse_args()
     return args
 
@@ -19,7 +22,14 @@ def load_image(_path: str) -> np.ndarray:
     im = cv2.cvtColor(im, cv2.COLOR_BGR2RGB)
     s = 96
     im = cv2.resize(im, (s, s))
+    return im
 
+
+def load_image_3D_CT(_path: str) -> np.ndarray:
+    im = cv2.imread(_path)
+    im = cv2.cvtColor(im, cv2.COLOR_BGR2RGB)
+    s = 96
+    im = cv2.resize(im, (s, s))
     return im
 
 
@@ -37,4 +47,21 @@ def load_model(_path: str, _n_channels: int, _n_classes: int):
     epoch = state['epoch']
     model.load_state_dict(state['model'])
     print('Restored model, epoch {}'.format(epoch))
+    return model
+
+
+def load_model_DO(_path: str, _n_channels: int, _n_classes: int):
+    model = UNet_DO(n_channels=_n_channels,
+                    n_classes=_n_classes)
+    state = torch.load(str(_path))
+    epoch = state['epoch']
+    model.load_state_dict(state['model'])
+    print('Restored model, epoch {}'.format(epoch))
+    return model
+
+
+def model_parallel(model):
+    if torch.cuda.device_count() > 1:
+        print("Let's use", torch.cuda.device_count(), "GPUs!")
+        model = nn.DataParallel(model)
     return model
